bd4cf90bb2287a42d44676a8996e8195a9b25b58
biljanaognjanov <biljanaognjanov1@gmail.com>
2023-12-14 18:55:56 +0100

    Inicijalni komit

---
+++ back-end/swt/src/main/java/swt/SwtApplication.java
@@ -0,0 +1,13 @@
+package swt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwtApplication.class, args);
+	}
+
+}
---
+++ back-end/swt/src/test/java/swt/SwtApplicationTests.java
@@ -0,0 +1,13 @@
+package swt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
